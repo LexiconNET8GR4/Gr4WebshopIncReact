6ccd20efde53ac70f403d3cfd7c42fc6bc6b6299
Ronja Mattsson <ronja_mattsson@outlook.com>
2022-07-12 15:12:48 +0100

    added support for uploading multiple images at once, and examples of this

--- Gr4WebshopIncReact/Controllers/FileUploadingController.cs
+++ Gr4WebshopIncReact/Controllers/FileUploadingController.cs
@@ -26,6 +26,7 @@ namespace Gr4WebshopIncReact.Controllers
         /// </summary>
         /// <param name="model">Wraps the file in a more easily modifibale class</param>
         /// <returns></returns>
+        [Route("UploadNewImage")]
         [HttpPost]
         public IActionResult UploadNewImage([FromForm] FileUploadingModel model)
         {
@@ -34,10 +35,42 @@ namespace Gr4WebshopIncReact.Controllers
             {
                 // Request validation of the uploaded file,
                 // Validating function also updates potential rejection message
-                if (ImageIsLegal(model, out rejectionMsg))
+                if (ImageIsLegal(model.File, out rejectionMsg))
                 {
                     // Once validated upload the file to the desination folder
-                    UploadedFile(model, imageDestination);
+                    UploadedFile(model.File, imageDestination);
+                }
+            }
+            return Json(rejectionMsg);
+        }
+
+        /// <summary>
+        /// Post function to be accessed from the frontend, inquires about upload validity before requesting it be saved locally
+        /// </summary>
+        /// <param name="model">Wraps the files in a more easily modifibale class</param>
+        /// <returns></returns>
+        [Route("UploadNewImages")]
+        [HttpPost]
+        public IActionResult UploadNewImages([FromForm] FilesUploadingModel model)
+        {
+            string rejectionMsg = "ModelState not valid"; // Return a message for result of action
+            if (ModelState.IsValid)
+            {
+                // For each files uploaded
+                // Request validation of the uploaded file,
+                // Validating function also updates potential rejection message
+                for (int i = 0; i < model.Files.Length; i++)
+                {
+                    // Halt process if even one file gets rejected
+                    if (ImageIsLegal(model.Files[i], out rejectionMsg))
+                    {
+                        // Once validated upload the file to the desination folder
+                        UploadedFile(model.Files[i], imageDestination);
+                    }
+                    else
+                    {
+                        return Json(rejectionMsg);
+                    }
                 }
             }
             return Json(rejectionMsg);
@@ -49,7 +82,7 @@ namespace Gr4WebshopIncReact.Controllers
         /// <param name="model">File to validate</param>
         /// <param name="rejectionMessage">An out param for returning an error message explaning the reasoning for the rejection, if any </param>
         /// <returns></returns>
-        private bool ImageIsLegal(FileUploadingModel model, out string rejectionMessage)
+        private bool ImageIsLegal(IFormFile model, out string rejectionMessage)
         {
             rejectionMessage = "No error detected"; // No inital errors detected
 
@@ -64,7 +97,7 @@ namespace Gr4WebshopIncReact.Controllers
 
             // File size check
             #region FileSize
-            if (model.File.Length > maxSizeBytes)
+            if (model.Length > maxSizeBytes)
             {
                 rejectionMessage = $"File size exceeds maximum allowed ({maxSizeBytes} bytes)";
                 return false;
@@ -76,7 +109,7 @@ namespace Gr4WebshopIncReact.Controllers
             bool legalFormatDetected = false;
             for (int i = 0; i < allowedFormats.Length; i++)
             {
-                if (string.Equals(model.File.ContentType, allowedFormats[i], StringComparison.OrdinalIgnoreCase))
+                if (string.Equals(model.ContentType, allowedFormats[i], StringComparison.OrdinalIgnoreCase))
                 {
                     legalFormatDetected = true;
                     break;
@@ -98,24 +131,18 @@ namespace Gr4WebshopIncReact.Controllers
             return true;
         }
 
-        private void UploadedFile(FileUploadingModel model, string destination)
+        private void UploadedFile(IFormFile model, string destination)
         {
-            string uniqueFileName = null;
-
-            // Run sanity check
-            if (model != null) 
-                return;
-          
             // Give the file a unique name by attaching a guid after the files name.
-            string nameUnextended = model.File.FileName.Split(".")[0];
-            string extension = model.File.FileName.Split(".")[1];
-            uniqueFileName = nameUnextended + "_" + Guid.NewGuid().ToString() + "." + extension;
+            string nameUnextended = model.FileName.Split(".")[0];
+            string extension = model.FileName.Split(".")[1];
+            string uniqueFileName = nameUnextended + "_" + Guid.NewGuid().ToString() + "." + extension;
 
             // Upload the file to the determined destination
             string filePath = Path.Combine(destination, uniqueFileName);
             using (var fileStream = new FileStream(filePath, FileMode.Create))
             {
-                model.File.CopyTo(fileStream);
+                model.CopyTo(fileStream);
             }
         }
 
