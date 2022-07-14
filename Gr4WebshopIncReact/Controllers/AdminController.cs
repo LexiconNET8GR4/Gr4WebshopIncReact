235c1e78ba2f0c5553fd88d4074088175cfbf133
Ronja Mattsson <ronja_mattsson@outlook.com>
2022-07-14 16:40:38 +0100

    admin uploading functions with axios in view

--- Gr4WebshopIncReact/Controllers/AdminController.cs
+++ Gr4WebshopIncReact/Controllers/AdminController.cs
@@ -5,11 +5,26 @@ using Gr4WebshopIncReact.Models;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Gr4WebshopIncReact.Models.viewModels;
 
 namespace Gr4WebshopIncReact.Controllers
 {
+    // Disabled during testing
+    //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private static AdminIndexVM indexVM;
+
+        public AdminController()
+        {
+            if (indexVM == null)
+            {
+                indexVM = new AdminIndexVM();
+            }
+            UpdateViewModel();
+        }
+
         public IActionResult Index()
         {
             List<string> images = Directory.GetFiles("wwwroot/uploaded_images").ToList();
@@ -18,8 +33,37 @@ namespace Gr4WebshopIncReact.Controllers
                 images[i] = images[i].Split("/")[1];
             }
 
-            return View(images);
+            return View(indexVM);
         }
+
+        private void UpdateViewModel()
+        {
+            // Ensure it exists
+            if (indexVM == null)
+            {
+                indexVM = new AdminIndexVM();
+            }
+
+            // Create a fresh file uploading container
+            indexVM.FilesUploader = new FilesUploadingModel();
+
+            // Get the image paths
+            List<string> images = Directory.GetFiles("wwwroot/uploaded_images").ToList();
+            for (int i = 0; i < images.Count; i++)
+            {
+                images[i] = images[i].Split("/")[1];
+            }
+
+            indexVM.ImageLocations = images;
+        }
+        [HttpPost]
+        public IActionResult TestingAxiosInPages()
+        {
+            return Json("Test recieved");
+        }
+
+        // Remove before push
+        #region Decripit
         public IActionResult UploadSingleImage()
         {
             return View("SingleForm", new FileUploadingModel());
@@ -29,6 +73,6 @@ namespace Gr4WebshopIncReact.Controllers
         {
             return View("MultipleForm", new FilesUploadingModel());
         }
-
+        #endregion
     }
 }
---
+++ Gr4WebshopIncReact/Models/viewModels/AdminIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gr4WebshopIncReact.Models.viewModels
+{
+    public class AdminIndexVM
+    {
+        public FilesUploadingModel FilesUploader { get; set; }
+        public List<string> ImageLocations { get; set; }
+    }
+}
